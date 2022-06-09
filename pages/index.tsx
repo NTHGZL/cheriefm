3127ce21cf30378a8e9b57e4cfae4072684b930f
Nathan Gonzalez <nathan.gonzalez@performanse.com>
2022-06-09 15:27:27 +0200

    add install button method

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,59 @@
+import { useEffect, useState } from 'react'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [deferredPrompt, setDeferredPrompt] = useState()
+
+  useEffect(()=> {
+    if("serviceWorker" in navigator) {
+      window.addEventListener("load", function () {
+       navigator.serviceWorker.register("/sw.js").then(
+          function (registration) {
+            console.log("Service Worker registration successful with scope: ", registration.scope);
+          },
+          function (err) {
+            console.log("Service Worker registration failed: ", err);
+          }
+        );
+      });
+    }
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      setDeferredPrompt(e)
+      
+      // Update UI notify the user they can install the PWA
+      // showInstallPromotion();
+      // Optionally, send analytics event that PWA install promo was shown.
+      console.log(`'beforeinstallprompt' event was fired.`);
+    });
+  }, [])
   return (
     <>
-      <header>
+      <div className='header'>
         Cherie FM
-      </header>
-      <body>
+      </div>
+      <div className='body'>
         Coucou
-        <button className='add-icon-button'>
-          <img className='add-icon' src='/install-icon.png' />
+        <div className='banner-add'>
+        <button className='add-icon-button' onClick={ async(e)=> {
+          
+          deferredPrompt.prompt()
+          if (deferredPrompt !== null) {
+            // deferredPrompt!.prompt();
+            // const { outcome } = await deferredPrompt.userChoice;
+            // if (outcome === 'accepted') {
+                // deferredPrompt = null;
+            // }
+        }
+        }}>
+          <img className='add-icon' src='/install-icon.png' /> Installer
         </button>
-      </body>
+        </div>
+        
+      </div>
     </>
   )
 }
